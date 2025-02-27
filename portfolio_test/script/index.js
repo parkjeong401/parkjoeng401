6e68c8d3a62e86e9fff78b6957e80a1e9cd6bd25
parkjeong401 <pjh96401@naver.com>
2025-02-27 12:23:15 +0900

    Add files via upload

---
+++ portfolio_test/script/index.js
@@ -0,0 +1,61 @@
+ //sns디자인
+ const snsslide = new Swiper('.sns', {
+    autoplay:{delay:0,},
+    loop:true,
+    speed:5000,
+    slide:5000,
+    slidesPerView:4,
+})
+
+//배너디자인
+const bnrlslide = new Swiper('.banner', {
+    autoplay:{delay:0,},
+    loop:true,
+    speed:5000,
+    slide:5000,
+    slidesPerView:3,
+})
+
+//상세디자인
+const detailslide = new Swiper('.detail')
+
+//출력 및 숨기기
+const popup_bg = document.querySelector('.popup_bg')
+const sns = document.querySelectorAll('.sns img')
+const bnr = document.querySelectorAll('.banner img')
+const detail = document.querySelectorAll('.detail img')
+
+popup_bg.style.display = 'none'
+for(let i of sns){
+    i.addEventListener('click',()=>{
+        popup_bg.style.display = 'block'
+        popup_bg.children[0].children[0].src = i.src;
+        popup_bg.children[0].style.width = '800px';
+        popup_bg.children[0].style.marginTop = '100px'
+        // console.log(i.src)
+        // console.log(popup_bg.children[0].children[0])
+    })
+}
+for(let i of bnr){
+    i.addEventListener('click',()=>{
+        popup_bg.style.display = 'block'
+        popup_bg.children[0].children[0].src = i.src;
+        popup_bg.children[0].style.width = '900px';
+        popup_bg.children[0].style.marginTop = '300px'
+        // console.log(i.src)
+        // console.log(popup_bg.children[0].children[0])
+    })
+}
+for(let i of detail){
+    i.addEventListener('click',()=>{
+        popup_bg.style.display = 'block'
+        popup_bg.children[0].children[0].src = i.src;
+        popup_bg.children[0].style.width = '900px';
+        popup_bg.children[0].style.marginTop = '100px'
+        // console.log(i.src)
+        // console.log(popup_bg.children[0].children[0])
+    })
+}
+popup_bg.addEventListener('click',function(){
+    popup_bg.style.display = 'none'
+})
